e8f2279238d3faca5c1e83b5205ff11fc4cc40bc
Mateus Bizzo <bizzo.mateus@gmail.com>
2020-04-16 09:58:20 -0300

    add Readme and initiating new game

---
+++ forca.py
@@ -0,0 +1,12 @@
+
+def jogar():
+    
+    print(45*'#')
+    print('##### Bem-vindo ao jogo Forca. #####')
+    print(45*'#')
+
+    print('Fim de Jogo')
+
+if __name__ == "__main__":
+    jogar()
+    
---
+++ jogos.py
@@ -0,0 +1,19 @@
+import forca
+import adivinha
+
+if __name__ == "__main__":
+    
+    print(30*'#')
+    print('##### Escolha seu jogo. #####')
+    print(30*'#')
+    
+    print('\n (1)Forca (2)Adivinhação: ')
+    jogo = input('Escolha: ')
+
+    if (jogo == 1):
+        forca.jogar()
+    elif (jogo == 2):
+        adivinha.jogar()
+    else:
+        print('Escolha inválida')
+        
